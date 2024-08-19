eb592d0de7e3b54c9a5751e8c21861e3ae8c8796
Reşat YAVÇİN <resatyavcin@Resat-MacBook-Pro.local>
2024-08-19 14:50:20 +0300

    Add google drive api

--- actions/serviceAdd.ts
+++ actions/serviceAdd.ts
@@ -1,15 +1,58 @@
 "use server"
 
 import * as z from "zod";
+import {google} from 'googleapis'
+
 import {CustomerVehicleServiceAddFormSchema} from '@/schemas'
 
 export const serviceAdd = async (values: z.infer<typeof CustomerVehicleServiceAddFormSchema>) => {
     const validatedFields = CustomerVehicleServiceAddFormSchema.safeParse(values);
 
-    console.log(values);
     if(!validatedFields.success){
         return { error: "Tip Hatası." }
     }
 
-    return { success: "Araç Dosyası oluşturuldu." }
+    const oauth2Client = new google.auth.OAuth2(
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        process.env.REDIRECT_URI
+    )
+    oauth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN})
+
+    try {
+
+        const fileMetadata = {
+            name:  values.customerName + " " + values.customerSurname,
+            mimeType: 'application/vnd.google-apps.folder',
+        };
+
+        const driveService = google.drive({
+            version: 'v3',
+            auth: oauth2Client
+        })
+
+        await driveService.files.create({
+            requestBody: fileMetadata,
+            fields: 'id',
+        }).then(async ({data})=>{
+
+            if(data.id){
+                const daats = await driveService.files.create({
+                    requestBody: {
+                        name:  values.customerVehicle + "-" + values.customerVehicleKM,
+                        mimeType: 'application/vnd.google-apps.folder',
+                        parents: [data.id]
+                    },
+                    fields: 'id',
+                })
+            }
+        })
+
+
+        return { success: "Araç Dosyası oluşturuldu." }
+    } catch (err) {
+            // TODO(developer) - Handle error
+        throw err;
+    }
+
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {cn} from "@/lib/utils";
-import { FaCarAlt } from "react-icons/fa";
-import CustomerActions from "@/components/customer/customer-actions";
 import * as React from "react";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,12 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+      <html lang="en">
       <body className={cn(inter.className)}>
-          <div vaul-drawer-wrapper="" className="bg-white min-h-[100vh]">
-              {children}
-          </div>
+      <div vaul-drawer-wrapper="" className="bg-white min-h-[100vh]">
+          {children}
+      </div>
       </body>
-    </html>
+      </html>
   );
 }
