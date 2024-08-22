12b093c12c1683c97904477e452fb23e03a2a183
resatyavcin <resatyavcin@icloud.com>
2024-08-22 13:12:14 +0300

    Try resolution props 5

--- app/customer/customer-detail/[fileId]/page.tsx
+++ app/customer/customer-detail/[fileId]/page.tsx
@@ -125,8 +125,6 @@ const CustomerFile = () => {
                         </Button>
                         <Camera
                             idealFacingMode={mode}
-                            idealResolution = {{width: 640, height: 480}}
-                            isFullscreen
                             onTakePhoto = { (dataUri) => { handleTakePhoto(dataUri); } }
                         />
                     </DialogContent>
