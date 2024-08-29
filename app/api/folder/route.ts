3d68ecd8a476a26be25a990c66832e064cd4c7c3
resatyavcin <resatyavcin@icloud.com>
2024-08-30 00:44:08 +0300

    log 4

--- app/api/folder/route.ts
+++ app/api/folder/route.ts
@@ -63,6 +63,6 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({status: 200, data: data.customerId, message: "Araç servise başarı ile kaydedilmiştir."})
 
     }catch (err){
-        return NextResponse.json({error: err, message: "Araç kaydetme başarısız. Lütfen tekrar deneyiniz."}, {status: 500});
+        return NextResponse.json({error: JSON.stringify(err), message: "Araç kaydetme başarısız. Lütfen tekrar deneyiniz."}, {status: 500});
     }
 }
